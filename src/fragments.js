b120a73c1a2f9c5db99969f0573b0f10fd7094fa
wooknick <wooknick@naver.com>
2019-04-23 22:58:54 +0900

    fix seeFullPost

--- src/fragments.js
+++ src/fragments.js
@@ -7,3 +7,38 @@ fragment CommentParts on Comment{
     }
 }
 `;
+
+export const USER_FRAGMENT = `
+fragment UserParts on User{
+    id
+    username
+}`;
+
+export const FILE_FRAGMENT = `
+fragment FileParts on File{
+    id
+    url
+}`;
+
+export const FULL_POST_FRAGMENT = `
+fragment PostParts on Post{
+    id
+    location
+    caption
+    files{
+        id
+        url
+    }
+    comments{
+        id
+        text
+        user{
+            username
+        }
+    }
+    user{
+        id
+        username
+    }
+}
+`;

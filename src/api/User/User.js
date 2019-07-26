16e4520cd853271c970a1ed3ac62b1ce626bc9ba
wooknick <wooknick@naver.com>
2019-07-27 02:54:26 +0900

    fix user query

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -5,6 +5,26 @@ export default {
         fullName: parent => {
             return `${parent.firstName} ${parent.lastName}`;
         },
+        isFollowing: async (parent, _, { request }) => {
+            const { user } = request;
+            const { id: parentId } = parent;
+            try {
+                return prisma.$exists.user({
+                    AND: [
+                        {
+                            id: user.id
+                        },
+                        {
+                            following_some: {
+                                id: parentId
+                            }
+                        }
+                    ]
+                });
+            } catch {
+                return false;
+            }
+        },
         isSelf: (parent, _, { request }) => {
             const { user } = request;
             const { id: parentId } = parent;
